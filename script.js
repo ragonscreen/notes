a2c3273ca0cab61ce78c026b0fd3b3c8ebb27680
Anurag Das <ragonscreen@gmail.com>
2023-10-01 16:16:42 +0530

    more functionality and better comments

--- script.js
+++ script.js
@@ -3,12 +3,23 @@
 */
 const DATABASE = [];
 
-const btnNewNote = document.querySelector('.btn-new-note');
+// Testing
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log(DATABASE);
+});
+
 const newNote = document.querySelector('.new-note');
+const btnNewNote = document.querySelector('.btn-new-note');
 const btnSave = document.querySelector('.btn-save');
 const btnCancel = document.querySelector('.btn-cancel');
 
+/**
+ * Main button functionalities.
+ *
+ */
 btnNewNote.addEventListener('click', () => {
+    resetNote();
     newNote.showModal();
 });
 
@@ -16,53 +27,54 @@ btnCancel.addEventListener('click', () => {
     newNote.close();
 });
 
-const container = document.querySelector('.container');
-
-// const displayNotes = (id, title, content) => {
-
-
-//     container.appendChild(element);
-// }
-
-
 btnSave.addEventListener('click', () => {
-    createNewNote()
-    resetNote();
+    createNewNote();
+    displayAlert('note created', 'success');
     newNote.close();
 });
 
+/**
+ * Creates a new note, and pushes the info into `DATABASE`.
+ * Also checks and replaces whether some special characters
+ * have been input, in which case, it replaces them with their
+ * escaped variants.
+*/
 
 const newNoteTitle = document.querySelector('.new-note__title');
 const newNoteContent = document.querySelector('.new-note__content');
-
 const createNewNote = () => {
     const noteId = new Date().getTime();
-    let noteTitle = '';
-    let noteContent = '';
+    let noteTitle = escapeChars(newNoteTitle.innerText);
+    let noteContent = escapeChars(newNoteContent.innerText);
+    console.log(noteContent);
 
-    noteTitle = newNoteTitle.innerText;
-    noteContent = newNoteContent.innerText;
+    if (noteContent) {
+        createNoteElement(noteId, noteTitle, noteContent);
 
-    let noteInfo = {
-        id: noteId,
-        title: noteTitle,
-        content: noteContent
+        let noteInfo = {
+            id: noteId,
+            title: noteTitle,
+            content: noteContent
+        }
+
+        DATABASE.unshift(noteInfo);
+        console.log(DATABASE);
     }
-
-    DATABASE.push(noteInfo);
-    createNoteElement(noteId, noteTitle, noteContent);
-    console.log(DATABASE);
 };
-
-const resetNote = () => {
-    newNoteTitle.textContent = '';
-    newNoteContent.textContent = '';
-}
 
 // DATABASE.forEach(note => {
 //     displayNotes(note.id, note.title, note.content);
 // });
 
+
+/**
+ * Creates a note element. (`div` with a class of `note` and
+ * a unique `data-id`).
+ * Based on content, also checks whether the note created
+ * should be large.
+*/
+
+const container = document.querySelector('.container');
 const createNoteElement = (id, title, content) => {
     const element = document.createElement('div');
     element.classList.add('note');
@@ -74,18 +86,15 @@ const createNoteElement = (id, title, content) => {
         <p class="note__content">${content}</p>`;
     element.innerHTML = noteHtml;
     container.prepend(element);
-}
+    const elementContent = element.querySelector('.note__content');
+    checkNoteLength(elementContent);
+};
 
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log(DATABASE);
-});
-
-
-
-
-
-// Display alert
+/**
+ * Displays an alert with custom text.
+ * Adds a class to the alert box based on the type of alert
+ * (info, success, danger).
+*/
 
 const alertBox = document.querySelector('.alert');
 const displayAlert = (text, action) => {
@@ -99,21 +108,59 @@ const displayAlert = (text, action) => {
 };
 
 /**
- * <----- General Improvements ----->
+ * <----- Utility ----->
+ *
+ * 01. Replace characters with their escape variants.
+ * 02. Replace escaped characters with their regular variants.
+ * 03. Reset `new-note` info.
+ * 04. Change note size based on content by toggling the `note-large` class.
+ * --> Note size is changed by identifying the pixel at the bottom
+ * of the text container and it's parent.
+*/
+
+// 01
+const escapeChars = text => {
+    return text.replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#39;");
+};
+
+// 02
+const unescapeChars = text => {
+    return text.replace(/&lt;/g, "<")
+        .replace(/&gt;/g, ">")
+        .replace(/&quot;/g, "\"")
+        .replace(/&#39;/g, "\'")
+        .replace(/&amp;/g, "&");
+};
+
+// 03
+const resetNote = () => {
+    newNoteTitle.textContent = '';
+    newNoteContent.textContent = '';
+};
+
+// 04
+const checkNoteLength = element => {
+    // 36 here is a bit of a magic number, for some reason the
+    // bottom of the element shows up as higher up than expected
+    const parent = element.parentElement;
+    return element.getBoundingClientRect().bottom + 36 > parent.getBoundingClientRect().bottom
+        ? parent.classList.add('note-large')
+        : parent.classList.remove('note-large');
+};
+
+/**
+ * <----- Secondary ----->
  *
  * 01. Add a `margin-top` to `main` based on the height of `nav`.
- *
  * 02. Add the `nav-movable` class to `nav`.
- * >> `nav-movable` adds a `box-shadow` and removes the `border-bottom`
+ * --> This adds a `box-shadow` and removes the `border-bottom`
  * by making it transparent.
- *
  * 03. Toggle fixed width list display by toggling the
  * `container-narrow` class on `.container`.
- *
- * 04. Change note size based on content by toggling the `note-large` class.
- * >> Note size is changed by identifying the pixel at the bottom
- * of the text container and it's parent.
- *
 */
 
 // 01
@@ -124,11 +171,9 @@ main.style.marginTop = `${navHeight / 16}rem`;
 
 // 02
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 1) {
-        nav.classList.add('nav-movable');
-    } else {
-        nav.classList.remove('nav-movable');
-    }
+    return window.scrollY > 1
+        ? nav.classList.add('nav-movable')
+        : nav.classList.remove('nav-movable');
 });
 
 // 03
@@ -137,14 +182,7 @@ btnView.addEventListener('click', () => {
     container.classList.toggle('container-narrow');
 });
 
-// 04
-const testElement = document.querySelector('.testing');
-const noteContents = document.querySelectorAll('.note__content');
-noteContents.forEach(noteContent => {
-    // 36 here is a bit of a magic number, for some reason the bottom of the element shows up as higher up than expected
-    if (noteContent.getBoundingClientRect().bottom + 36 > noteContent.parentElement.getBoundingClientRect().bottom) {
-        noteContent.parentElement.classList.add('note-large');
-    } else {
-        noteContent.parentElement.classList.remove('note-large');
-    }
-});
+// later on DOMcontentloaded ->
+
+// const noteContents = document.querySelectorAll('.note__content');
+// noteContents.forEach(noteContent => checkLength(noteContent));
